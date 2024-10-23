b735ec784d1a1729d7700d1792de14e4092116bb
yuran-nhassengo <yurannhassengo@gmail.com>
2024-10-23 21:03:13 +0200

    testing blade

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,11 @@
-<h1>Tela de Produtos,{{$produto}}</h1>
+<h1>Tela de Produtos</h1>
+
+<main>
+    @if($produto == "Banana")
+        <p>Escolheu uma Banana</p>
+    @elseif(@produto == "Laranja")
+        <p>Escolheu uma Laranja</p>
+    @else
+        <p>Opcao Invalidade</p>
+    @endif
+</main>
