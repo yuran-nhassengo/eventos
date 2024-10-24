bf8d3c5e0bf3e0d8a72cd5550977e5589992b48d
yuran-nhassengo <yurannhassengo@gmail.com>
2024-10-24 20:52:58 +0200

    add navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,6 +12,39 @@
 
     </head>
     <body >
+
+        <header>
+            <nav class='navbar navbar-expand-lg navbar-light'>
+                <div class="collape navbar-collapse" id="navbar">
+                    <a href="" class="navbar-brand">
+                        <img src="/img/hdcevents_logo.svg" alt="logo">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                Eventos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                Criar Eventos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                Entrar
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                Cadastrar
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
         @yield('content')
         <footer>
             <p>HDC Events &copy; 2024</p>
